d7bbfac8bf1763d4db22e5976611b59abdbcecec
Clayton Lengel-Zigich <Clayton Lengel-Zigich clayton@claytonlz.com>
2011-08-18 16:31:04 -0700

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Multidb::Application.config.secret_token = 'b69fadbe5d547d750d77c368670ed08b6e210086a2f852a66ebc8a40c0e7a5269cfadb2160a4959aeb6954b6e3ef8f496bbbde249b36a50c1882214df3dcd39c'
